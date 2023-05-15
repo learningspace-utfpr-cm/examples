e427df963e401fc1506dab7cb7d382cdcc0e9dd8
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2023-05-15 17:10:06 -0300

    feat: permitindo utilizacao de delimitador personalizado
    Closes: 4

--- Reverseword.java
+++ Reverseword.java
@@ -8,38 +8,48 @@
  * Output: "s'teL ekat edoCteeL tsetnoc"
  */
 public class Reverseword {
-    public String reverseWords(String s) 
-    {       
-        char [] str = s.toCharArray();
+
+    char delimiter;
+
+    Reverseword(char delimiter) {
+        this.setDelimiter(delimiter);
+    }
+
+    public String reverseWords(String s) {
+        char[] str = s.toCharArray();
         int i = 0;
-        
-        for(int j=0; j < str.length; j++)
-        {
-            if(str[j] == ' ')
-            {
-                reverse(str, i, j-1);
-                i = j+1; 
+
+        for (int j = 0; j < str.length; j++) {
+            if (str[j] == this.getDelimiter()) {
+                reverse(str, i, j - 1);
+                i = j + 1;
             }
         }
-        reverse(str, i, str.length-1);
-        return new String(str);        
+        reverse(str, i, str.length - 1);
+        return new String(str);
     }
-    
-    public void reverse(char [] str, int l, int r)
-    {
-        while( l < r )
-        {
+
+    public void reverse(char[] str, int l, int r) {
+        while (l < r) {
             char temp = str[l];
             str[l] = str[r];
             str[r] = temp;
-            l++; r--;
+            l++;
+            r--;
         }
     }
 
-   public static void main(String[] args) {
-    Reverseword rw = new Reverseword();
-    System.out.println("Input: " + args[0]);
-    System.out.println("Output: " + rw.reverseWords(args[0]));
-   }
-}
+    public char getDelimiter() {
+        return this.delimiter;
+    }
 
+    public void setDelimiter(char delimiter) {
+        this.delimiter = delimiter;
+    }
+
+    public static void main(String[] args) {
+        Reverseword rw = new Reverseword('*');
+        System.out.println("Input: " + args[0]);
+        System.out.println("Output: " + rw.reverseWords(args[0]));
+    }
+}
