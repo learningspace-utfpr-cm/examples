70a66e20e440263560704037e024e5d47295e902
jhonatancunha <jhonatancunha@alunos.utfpr.edu.br>
2023-05-15 17:22:10 -0300

    fix: desfazendo alteracao da estilizacao de codigo

--- Reverseword.java
+++ Reverseword.java
@@ -11,45 +11,53 @@ public class Reverseword {
 
     char delimiter;
 
-    Reverseword(char delimiter) {
+    Reverseword(char delimiter) 
+    {
         this.setDelimiter(delimiter);
     }
 
-    public String reverseWords(String s) {
-        char[] str = s.toCharArray();
-        int i = 0;
-
-        for (int j = 0; j < str.length; j++) {
-            if (str[j] == this.getDelimiter()) {
-                reverse(str, i, j - 1);
-                i = j + 1;
-            }
-        }
-        reverse(str, i, str.length - 1);
-        return new String(str);
-    }
-
-    public void reverse(char[] str, int l, int r) {
-        while (l < r) {
-            char temp = str[l];
-            str[l] = str[r];
-            str[r] = temp;
-            l++;
-            r--;
-        }
-    }
-
-    public char getDelimiter() {
+    public char getDelimiter() 
+    {
         return this.delimiter;
     }
 
-    public void setDelimiter(char delimiter) {
+    public void setDelimiter(char delimiter) 
+    {
         this.delimiter = delimiter;
     }
 
-    public static void main(String[] args) {
-        Reverseword rw = new Reverseword('*');
-        System.out.println("Input: " + args[0]);
-        System.out.println("Output: " + rw.reverseWords(args[0]));
+    public String reverseWords(String s) 
+    {       
+        char [] str = s.toCharArray();
+        int i = 0;
+        
+        for(int j=0; j < str.length; j++)
+        {
+            if(str[j] == ' ')
+            {
+                reverse(str, i, j-1);
+                i = j+1; 
+            }
+        }
+        reverse(str, i, str.length-1);
+        return new String(str);        
     }
+    
+    public void reverse(char [] str, int l, int r)
+    {
+        while( l < r )
+        {
+            char temp = str[l];
+            str[l] = str[r];
+            str[r] = temp;
+            l++; r--;
+        }
+    }
+
+   public static void main(String[] args) {
+    Reverseword rw = new Reverseword('*');
+    System.out.println("Input: " + args[0]);
+    System.out.println("Output: " + rw.reverseWords(args[0]));
+   }
 }
+
