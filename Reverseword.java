7317f74a24ad6da4cc331d098f363b2ba369b66c
joaokorcz <joaokorcz@hotmail.com>
2024-03-12 21:09:25 -0300

    fix: use correct arg logging input

--- Reverseword.java
+++ Reverseword.java
@@ -38,7 +38,7 @@ public class Reverseword {
 
    public static void main(String[] args) {
     Reverseword rw = new Reverseword();
-    System.out.println("Input: " + args[1]);
+    System.out.println("Input: " + args[0]);
     System.out.println("Output: " + rw.reverseWords(args[0]));
    }
 }
